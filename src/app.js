7cc78992c3b239df073b3ec743a0ad21770e1867
Raamsrupesh <raamsrupesh@example.com>
2026-08-28 21:07:41 +0530

    remove Swagger API documentation

--- src/app.js
+++ src/app.js
@@ -1,23 +1,13 @@
 import express from "express";
-import swaggerUi from "swagger-ui-express";
-import swaggerSpec from "./config/swagger.js";
 import authRouter from './routes/auth.routes.js'
 import studentRouter from './routes/student.routes.js';
-import autodriverRouter from './routes/autodriver.route.js';
+import autodriverRouter from './routes/autodriver.routes.js';
 import isLoggedIn from './middlewares/auth.middleware.js';
 import {error_response} from './utils/error-response.js';
 import isAutoDriver from './middlewares/autodriver.middleware.js'
 import isStudent from './middlewares/student.middleware.js'
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(
-	"/api-docs",
-	swaggerUi.serve,
-	swaggerUi.setup(swaggerSpec)
-);
 
 app.use("/api/auth/v1/", authRouter);
 app.use("/api/student/v1", isLoggedIn, isStudent, studentRouter);
